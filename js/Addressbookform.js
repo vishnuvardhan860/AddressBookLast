f3e5aa0be475d1b3e29716b2e419209b2bb25828
vishnuvardhan860 <vishnuvardhan860@gmail.com>
2021-09-06 19:59:01 +0530

    Added: Create json object of contact data

--- js/Addressbookform.js
+++ js/Addressbookform.js
@@ -71,3 +71,33 @@ window.addEventListener('DOMContentLoaded', (event) => {
   
     });
   });
+  
+  const save = (event) => {
+    event.preventDefault();
+    let contactData = createContact();
+    let jsonObject = JSON.stringify(contactData);
+    alert(jsonObject);
+  }
+  
+  const createContact = () => {
+    let contactData = new Contact();
+    let names = getInputValueById('#name').split(" ");
+    contactData.firstName = names[0];
+    contactData.lastName = names[1];
+    contactData.address = getInputValueById('#address');
+    contactData.city = getInputValueById('#city');
+    contactData.state = getInputValueById('#state');
+    contactData.zip = getInputValueById('#zip');
+    contactData.phone = getInputValueById('#phone');
+    contactData.email = getInputValueById('#email');
+    return contactData;
+  }
+  
+  const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+  }
+  
+  const resetForm = () => {
+    
+  }
